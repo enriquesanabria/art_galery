8d2c752435ae2ea54e7fc31ef6b67444e1f7d1b1
Kike <en.sanabria14@gmail.com>
2023-08-10 16:10:17 -0300

    se agrega componente para las imagenes , verificar y pensar bien , necesito un componente que contenga la imagen y la descripción con disposición por lado , izq y derecha en Artista

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,13 +1,37 @@
 import Navegation from '../Layouts/Navegation';
+import Footer from '../Layouts/Footer';
+import ImageComponent from '../Layouts/ImageComponent';
+
+import artist1 from '../assets/img/artist/Artist1.jpg';
+import artist2 from '../assets/img/artist/Artist2.jpg';
+import artist3 from '../assets/img/artist/Artist3.jpg';
+import artist4 from '../assets/img/artist/Artist4.jpg';
+
+import { Container } from 'react-bootstrap';
 
 export default function Artist() {
 	return (
 		<div>
 			<Navegation />
-			<h1>Prueba</h1>
-			<h1>Prueba</h1>
-			<h1>Prueba</h1>
-			<h1>Prueba</h1>
+			<Container fluid>
+				<ImageComponent
+					imageUrl={artist1}
+					altText='Descripción de la imagen 1'
+				/>
+				<ImageComponent
+					imageUrl={artist2}
+					altText='Descripción de la imagen 2'
+				/>
+				<ImageComponent
+					imageUrl={artist3}
+					altText='Descripción de la imagen 3'
+				/>
+				<ImageComponent
+					imageUrl={artist4}
+					altText='Descripción de la imagen 4'
+				/>
+			</Container>
+			<Footer />
 		</div>
 	);
 }
