7abf92931b6270c5f02fba6c7175e36f07e803c3
Kike <en.sanabria14@gmail.com>
2023-08-10 12:05:16 -0300

    se añaden iconos en el footer , se soluciona problema con fontawesome

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,5 +1,8 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import IconMeva from '../assets/img/icons/iconMeva.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
 	return (
@@ -12,16 +15,23 @@ export default function Footer() {
 								href='mailto:tripoli.arte@gmail.com?Subject=Cuadros%20de%20su%20página'
 								id='correo'
 							></a>
+							<FontAwesomeIcon icon={faEnvelope} />
 							<p className='pt-1 px-2'>tripoli.arte@gmail.com</p>
 							<a
 								href='https://www.instagram.com/tripoli.maria.laura/'
 								className='text-dark mx-2'
-							></a>
+							>
+								instagram
+							</a>
+							<FontAwesomeIcon icon={faInstagram} />
 							<p className='pt-1 px-1'>@tripoli.maria.laura</p>
 							<a
 								href='https://wa.me/111566315443?text=Hola!%20estoy%20interesad@%20en%20un%20cuadro'
 								className='text-dark mx-2'
-							></a>
+							>
+								whatsapp
+							</a>
+							<FontAwesomeIcon icon={faWhatsapp} />
 							<p className='pt-1 px-1'>+54 9 1166315443</p>
 						</Col>
 
