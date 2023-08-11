9de99479cefbbba76e975822269fdf7f66b25e4b
Kike <en.sanabria14@gmail.com>
2023-08-11 12:06:37 -0300

    termine de maquetar componente de apartado artista , todavia falta teminar y aun se puede mejorar/modularizar aún más

--- src/Layouts/DescriptionAndImage.jsx
+++ src/Layouts/DescriptionAndImage.jsx
@@ -1,1 +1,30 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
+function MiComponente(props) {
+	return (
+		<Container fluid>
+			<Row className={`d-flex align-items-center ${props.flexRow}`}>
+				<Col
+					sm={4}
+					className={`d-flex justify-content-center p-0 ${props.justifyContent}`}
+				>
+					<Image
+						loading='lazy'
+						src={props.imagenProp}
+						alt='Foto artista 1'
+						fluid
+					/>
+				</Col>
+
+				<Col
+					sm={8}
+					className={`d-flex justify-content-center p-0 ${props.justifyContent}`}
+				>
+					{props.parrafo}
+				</Col>
+			</Row>
+		</Container>
+	);
+}
+
+export default MiComponente;
--- src/Layouts/ImageComponent.jsx
+++
@@ -1,7 +0,0 @@
-import { Image } from 'react-bootstrap';
-
-// eslint-disable-next-line react/prop-types
-const ImageComponent = ({ imageUrl, altText }) => {
-	return <Image src={imageUrl} alt={altText} fluid />;
-};
-export default ImageComponent;
