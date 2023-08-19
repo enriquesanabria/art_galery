79bf8dab5a31fa2e01756be44eebf7ba2d10a34e
Kike <en.sanabria14@gmail.com>
2023-08-19 13:39:17 -0300

    terminamos de añadir contenido de portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -620,6 +620,358 @@ export default function Portfolio() {
 					</div>
 				}
 			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Calm}
+				description={
+					<div>
+						{description.Calm.title}
+						<br />
+						{description.Calm.location}
+						<br />
+						{description.Calm.style}
+						<br />
+						{description.Calm.size}
+						<br />
+						{description.Calm.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Underworld}
+				description={
+					<div>
+						{description.Underworld.title}
+						<br />
+						{description.Underworld.location}
+						<br />
+						{description.Underworld.style}
+						<br />
+						{description.Underworld.size}
+						<br />
+						{description.Underworld.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Vision}
+				description={
+					<div>
+						{description.Vision.title}
+						<br />
+						{description.Vision.location}
+						<br />
+						{description.Vision.style}
+						<br />
+						{description.Vision.size}
+						<br />
+						{description.Vision.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Parrots}
+				description={
+					<div>
+						{description.Parrots.title}
+						<br />
+						{description.Parrots.location}
+						<br />
+						{description.Parrots.style}
+						<br />
+						{description.Parrots.size}
+						<br />
+						{description.Parrots.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Pallete}
+				description={
+					<div>
+						{description.Palette.title}
+						<br />
+						{description.Palette.location}
+						<br />
+						{description.Palette.style}
+						<br />
+						{description.Palette.size}
+						<br />
+						{description.Palette.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Cement}
+				description={
+					<div>
+						{description.Cement.title}
+						<br />
+						{description.Cement.location}
+						<br />
+						{description.Cement.style}
+						<br />
+						{description.Cement.size}
+						<br />
+						{description.Cement.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Cyan1}
+				description={
+					<div>
+						{description.Cyan1.title}
+						<br />
+						{description.Cyan1.location}
+						<br />
+						{description.Cyan1.style}
+						<br />
+						{description.Cyan1.size}
+						<br />
+						{description.Cyan1.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Cyan2}
+				description={
+					<div>
+						{description.Cyan2.title}
+						<br />
+						{description.Cyan2.location}
+						<br />
+						{description.Cyan2.style}
+						<br />
+						{description.Cyan2.size}
+						<br />
+						{description.Cyan2.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Colors}
+				description={
+					<div>
+						{description.Colors.title}
+						<br />
+						{description.Colors.location}
+						<br />
+						{description.Colors.style}
+						<br />
+						{description.Colors.size}
+						<br />
+						{description.Colors.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Frida}
+				description={
+					<div>
+						{description.Frida.title}
+						<br />
+						{description.Frida.location}
+						<br />
+						{description.Frida.style}
+						<br />
+						{description.Frida.size}
+						<br />
+						{description.Frida.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Aqua}
+				description={
+					<div>
+						{description.Aqua.title}
+						<br />
+						{description.Aqua.location}
+						<br />
+						{description.Aqua.style}
+						<br />
+						{description.Aqua.size}
+						<br />
+						{description.Aqua.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Shining}
+				description={
+					<div>
+						{description.Shining.title}
+						<br />
+						{description.Shining.location}
+						<br />
+						{description.Shining.style}
+						<br />
+						{description.Shining.size}
+						<br />
+						{description.Shining.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Birds}
+				description={
+					<div>
+						{description.Birds.title}
+						<br />
+						{description.Birds.location}
+						<br />
+						{description.Birds.style}
+						<br />
+						{description.Birds.size}
+						<br />
+						{description.Birds.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Souls}
+				description={
+					<div>
+						{description.Souls.title}
+						<br />
+						{description.Souls.location}
+						<br />
+						{description.Souls.style}
+						<br />
+						{description.Souls.size}
+						<br />
+						{description.Souls.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Ryrhm}
+				description={
+					<div>
+						{description.Ryrhm.title}
+						<br />
+						{description.Ryrhm.location}
+						<br />
+						{description.Ryrhm.style}
+						<br />
+						{description.Ryrhm.size}
+						<br />
+						{description.Ryrhm.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Moss}
+				description={
+					<div>
+						{description.Moss.title}
+						<br />
+						{description.Moss.location}
+						<br />
+						{description.Moss.style}
+						<br />
+						{description.Moss.size}
+						<br />
+						{description.Moss.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Mosaic}
+				description={
+					<div>
+						{description.Mosaic.title}
+						<br />
+						{description.Mosaiclocation}
+						<br />
+						{description.Mosaic.style}
+						<br />
+						{description.Mosaic.size}
+						<br />
+						{description.Mosaic.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='text-start'
+				texto='x'
+				imagenProp={cuadros.Hole}
+				description={
+					<div>
+						{description.Hole.title}
+						<br />
+						{description.Hole.location}
+						<br />
+						{description.Hole.style}
+						<br />
+						{description.Hole.size}
+						<br />
+						{description.Hole.status}
+					</div>
+				}
+			/>
+			<DescriptionAndImage
+				flexRow='flex-sm-row-reverse'
+				justifyContent='justify-content-sm-end'
+				texto='x'
+				imagenProp={cuadros.Beat}
+				description={
+					<div>
+						{description.Beat.title}
+						<br />
+						{description.Beat.location}
+						<br />
+						{description.Beat.style}
+						<br />
+						{description.Beat.size}
+						<br />
+						{description.Beat.status}
+					</div>
+				}
+			/>
 			<Footer />
 		</div>
 	);
