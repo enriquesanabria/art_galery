220ecbc2395897e8607428b4d932622b6634d7b3
Kike <en.sanabria14@gmail.com>
2023-09-08 14:16:32 -0300

    arreglamos footer

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -10,35 +10,35 @@ export default function Footer() {
 	return (
 		<footer>
 			<Container fluid>
-				<Row>
-					<Col sm={8} className={`d-block d-sm-flex text-start ${style.col}`}>
-						<div>
+				<Row className={'align-items-center mt-2'}>
+					<Col sm={8} className={'d-sm-flex text-center'}>
+						<div className={'d-sm-flex ms-3'}>
 							<a
 								href='mailto:tripoli.arte@gmail.com?Subject=Cuadros%20de%20su%20página'
 								id='correo'
-								className={`${style.a}`}
+								className={style.a}
 							>
 								<FontAwesomeIcon icon={faEnvelope} className={style.icon} />
-								<p className={style.inline_text}>tripoli.arte@gmail.com</p>
 							</a>
+							<p className='m-1'>tripoli.arte@gmail.com</p>
 						</div>
-						<div>
+						<div className={'d-sm-flex ms-3'}>
 							<a
 								href='https://www.instagram.com/tripoli.maria.laura/ '
-								className={`${style.a}`}
+								className={style.a}
 							>
 								<FontAwesomeIcon icon={faInstagram} className={style.icon} />
-								<p className={style.inline_text}>@tripoli.maria.laura</p>
 							</a>
+							<p className='m-1'>@tripoli.maria.laura</p>
 						</div>
-						<div>
+						<div className={'d-sm-flex ms-3'}>
 							<a
 								href='https://wa.me/111566315443?text=Hola!%20estoy%20interesad@%20en%20un%20cuadro'
-								className={`${style.a}`}
+								className={style.a}
 							>
 								<FontAwesomeIcon icon={faWhatsapp} className={style.icon} />
-								<p className={style.inline_text}>+54 9 1166315443</p>
 							</a>
+							<p className='m-1'>+54 9 1166315443</p>
 						</div>
 					</Col>
 
