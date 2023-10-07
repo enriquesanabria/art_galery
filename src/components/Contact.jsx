32517d2cf8552c10919bc545062c9d31840c8bad
Kike <en.sanabria14@gmail.com>
2023-10-06 21:11:10 -0300

    arreglamos el footer en el pie de pagina

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Container, Nav } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 import { contacto } from '../Layouts/ImageImporter';
 
@@ -16,47 +16,49 @@ const imagesSet1 = [contacto.cuadro1, contacto.cuadro2, contacto.cuadro3];
 const ContactCarousel = () => {
 	return (
 		<Container fluid className='base'>
-			<Navegation />
-			<Row>
-				<Col md={6}>
-					<ComponentCarousel images={imagesSet1} />
-				</Col>
-				<Col
-					md={6}
-					className='d-flex justify-content-center align-items-center'
-				>
-					<ul>
-						<li>
-							<a
-								href='https://wa.me/111566315443?text=Hola!%20estoy%20interesad@%20en%20un%20cuadro'
-								className={`d-flex ${style.link}`}
-							>
-								<FontAwesomeIcon icon={faWhatsapp} />
-								<p className={style.contact}>+54 9 1166315443</p>
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://www.instagram.com/tripoli.maria.laura/'
-								className={`d-flex ${style.link}`}
-							>
-								<FontAwesomeIcon icon={faInstagram} />
-								<p className={style.contact}>tripoli.maria.laura</p>
-							</a>
-						</li>
-						<li>
-							<a
-								href='https://wa.me/111566315443?text=Hola!%20estoy%20interesad@%20en%20un%20cuadro'
-								className={`d-flex ${style.link}`}
-							>
-								<FontAwesomeIcon icon={faEnvelope} />
-								<p className={style.contact}>+54 9 1166315443</p>
-							</a>
-						</li>
-					</ul>
-				</Col>
-				<Footer page={true} />
-			</Row>
+			<main className='content'>
+				<Navegation />
+				<Row>
+					<Col md={6}>
+						<ComponentCarousel images={imagesSet1} />
+					</Col>
+					<Col
+						md={6}
+						className='d-flex justify-content-center align-items-center'
+					>
+						<ul className={style.listaContacto}>
+							<li>
+								<a
+									href='https://wa.me/111566315443?text=Hola!%20estoy%20interesad@%20en%20un%20cuadro'
+									className={`d-flex ${style.link}`}
+								>
+									<FontAwesomeIcon icon={faWhatsapp} />
+									<p className={style.contact}>+54 9 1166315443</p>
+								</a>
+							</li>
+							<li>
+								<a
+									href='https://www.instagram.com/tripoli.maria.laura/'
+									className={`d-flex ${style.link}`}
+								>
+									<FontAwesomeIcon icon={faInstagram} />
+									<p className={style.contact}>tripoli.maria.laura</p>
+								</a>
+							</li>
+							<li>
+								<a
+									href='https://wa.me/111566315443?text=Hola!%20estoy%20interesad@%20en%20un%20cuadro'
+									className={`d-flex ${style.link}`}
+								>
+									<FontAwesomeIcon icon={faEnvelope} />
+									<p className={style.contact}>tripoli.arte@gmail.com</p>
+								</a>
+							</li>
+						</ul>
+					</Col>
+				</Row>
+			</main>
+			<Footer page={true} />
 		</Container>
 	);
 };
